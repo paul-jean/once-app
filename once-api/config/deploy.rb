f53485f5c95d014610297a725b326ba0ddbaea3e
Paul-Jean Letourneau <paul.jean.letourneau@gmail.com>
2019-02-14 14:07:18 -0800

    setup postgres db in prod

--- once-api/config/deploy.rb
+++ once-api/config/deploy.rb
@@ -1,5 +1,7 @@
+# frozen_string_literal: true
+
 # Change these
-server '134.209.4.183', port: 22, roles: [:web, :app, :db], primary: true
+server '134.209.4.183', port: 22, roles: %i[web app db], primary: true
 
 set :repo_url,        'git@github.com:paul-jean/once-app.git'
 set :application,     'onceapp'
@@ -18,10 +20,10 @@ set :puma_state,      "#{shared_path}/tmp/pids/puma.state"
 set :puma_pid,        "#{shared_path}/tmp/pids/puma.pid"
 set :puma_access_log, "#{release_path}/log/puma.error.log"
 set :puma_error_log,  "#{release_path}/log/puma.access.log"
-set :ssh_options,     { forward_agent: true, user: fetch(:user), keys: %w(~/.ssh/id_rsa) }
+set :ssh_options,     forward_agent: true, user: fetch(:user), keys: %w[~/.ssh/id_rsa]
 set :puma_preload_app, true
 set :puma_worker_timeout, nil
-set :puma_init_active_record, true  # Change to false when not using ActiveRecord
+set :puma_init_active_record, true # Change to false when not using ActiveRecord
 
 ## Defaults:
 # set :scm,           :git
@@ -47,12 +49,12 @@ namespace :puma do
 end
 
 namespace :deploy do
-  desc "Make sure local git is in sync with remote."
+  desc 'Make sure local git is in sync with remote.'
   task :check_revision do
     on roles(:app) do
       unless `git rev-parse HEAD` == `git rev-parse origin/master`
-        puts "WARNING: HEAD is not the same as origin/master"
-        puts "Run `git push` to sync changes."
+        puts 'WARNING: HEAD is not the same as origin/master'
+        puts 'Run `git push` to sync changes.'
         exit
       end
     end
@@ -73,8 +75,8 @@ namespace :deploy do
     end
   end
 
- # before :starting,     :check_revision
-  after  :finishing,    :compile_assets
+  before :starting,     :check_revision
+  # after  :finishing,    :compile_assets
   after  :finishing,    :cleanup
   after  :finishing,    :restart
 end
