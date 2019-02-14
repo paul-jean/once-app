c577869d8d0e7b7bcece83e0c09d854bafd7a69c
Paul-Jean Letourneau <paul.jean.letourneau@gmail.com>
2019-02-13 20:58:09 -0800

    Specify deploy subdir in capfile. Use postgres in dev

--- once-api/config/deploy.rb
+++ once-api/config/deploy.rb
@@ -1,9 +1,9 @@
 # Change these
-server '134.209.4.183', port: 80, roles: [:web, :app, :db], primary: true
+server '134.209.4.183', port: 22, roles: [:web, :app, :db], primary: true
 
 set :repo_url,        'git@github.com:paul-jean/once-app.git'
 set :application,     'onceapp'
-set :user,            'rails'
+set :user,            'deploy'
 set :puma_threads,    [4, 16]
 set :puma_workers,    0
 
@@ -18,7 +18,7 @@ set :puma_state,      "#{shared_path}/tmp/pids/puma.state"
 set :puma_pid,        "#{shared_path}/tmp/pids/puma.pid"
 set :puma_access_log, "#{release_path}/log/puma.error.log"
 set :puma_error_log,  "#{release_path}/log/puma.access.log"
-set :ssh_options,     { forward_agent: true, user: fetch(:user), keys: %w(~/.ssh/id_rsa.pub) }
+set :ssh_options,     { forward_agent: true, user: fetch(:user), keys: %w(~/.ssh/id_rsa) }
 set :puma_preload_app, true
 set :puma_worker_timeout, nil
 set :puma_init_active_record, true  # Change to false when not using ActiveRecord
@@ -73,7 +73,7 @@ namespace :deploy do
     end
   end
 
-  before :starting,     :check_revision
+ # before :starting,     :check_revision
   after  :finishing,    :compile_assets
   after  :finishing,    :cleanup
   after  :finishing,    :restart
--- once-api/db/schema.rb
+++ once-api/db/schema.rb
@@ -12,7 +12,10 @@
 
 ActiveRecord::Schema.define(version: 2019_02_08_002940) do
 
-  create_table "posts", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
+  create_table "posts", force: :cascade do |t|
     t.string "title"
     t.text "body"
     t.datetime "created_at", null: false
